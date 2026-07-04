76a94459699cd89f8240f4d4ac707182a8058ddc
Nirosha053 <nirosham.cse2023@dce.edu.in>
2026-07-04 14:02:14 +0530

    Time: 3 ms (92.84%), Space: 46.4 MB (33.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        int[] counts = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            counts[s.charAt(i) - 'a']++;
-            counts[t.charAt(i) - 'a']--;
-        }
-    for (int count : counts) {
-            if (count != 0) return false;
-        }
-
-        return true;
+      char []a=s.toCharArray();
+       char []b=t.toCharArray();
+       Arrays.sort(a);
+       Arrays.sort(b);
+       return Arrays.equals(a,b);
     }
 }
