663d26795d7dd809cace4251bf51fc9b1601c02d
Nirosha053 <nirosham.cse2023@dce.edu.in>
2026-07-09 12:14:58 +0530

    Time: 320 ms (14.87%), Space: 61 MB (5.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,38 @@
-import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int i;
-        List<List<Integer>>finalAns=new ArrayList<>();
-        HashSet <List<Integer>>h1=new HashSet<>();
+        int i,j,k;
+        int numsLength = nums.length;
+        List<List<Integer>> finalAns = new ArrayList<>();
+        HashSet<List<Integer>> h1 = new HashSet<>();
         Arrays.sort(nums);
-        for(i=0;i<nums.length-2;i++){
-            
+            for(i = 0;i<nums.length-1;i++){
                 int start=i+1;
-                int end=nums.length-1;
+                int end=numsLength -1;
                 while(start<end){
-                    int sum=nums[i]+nums[start]+nums[end];
+                    int sum = nums[i]+nums[start]+nums[end];
                     if(sum==0){
-                        List<Integer> l1=new ArrayList<>();
+                        List<Integer>l1=new ArrayList<>();
                         l1.add(nums[i]);
-                         l1.add(nums[start]);
-                          l1.add(nums[end]);
-                          h1.add(l1);
-                          start++;
-                          end--;
+                        l1.add(nums[start]);
+                        l1.add(nums[end]);
+                        h1.add(l1);
+                        start++;
+                        end--;
                     }
-                    else if(sum<0){
+                    else if (sum<0){
                         start++;
                     }
                     else{
                         end--;
                     }
                 }
-                  
+            }
+        
+        for(var it : h1)
+        {
+            finalAns.add(it);
         }
-  for(var it:h1){
-    finalAns.add(it);
-  }
-  return finalAns;
+        return finalAns;
+        
     }
 }
