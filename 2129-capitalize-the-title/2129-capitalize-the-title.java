2c1810824a8bd9c0cfd4a0459f516b8fe69b32f6
Nirosha053 <nirosham.cse2023@dce.edu.in>
2026-07-12 19:38:36 +0530

    Time: 5 ms (47.4%), Space: 44 MB (38.1%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i].toLowerCase();
+            if (word.length() >= 3) {
+                word = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+            }
+            result.append(word);
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+        
+        return result.toString();
+    }
+}
