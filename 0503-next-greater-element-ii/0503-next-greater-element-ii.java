3bf640706fbd1188f81bfb806b44b3fb09115df1
Nirosha053 <nirosham.cse2023@dce.edu.in>
2026-07-12 19:25:18 +0530

    Time: 3 ms (99.31%), Space: 48.3 MB (69.38%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        Deque<Integer> stack = new ArrayDeque<>(); // stores indices, values decreasing
+
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                result[stack.pop()] = num;
+            }
+            // Only push indices during first pass to avoid redundant work
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return result;
+    }
+}
