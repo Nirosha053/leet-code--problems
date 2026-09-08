e715131c15a230bdc7c3dd82a15874899a097a13
Nirosha053 <nirosham.cse2023@dce.edu.in>
2026-09-08 16:30:28 +0530

    Time: 12 ms (99.9%), Space: 42.3 MB (17.18%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,21 +3,21 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1;
-        int right = n;
+        int low = 1;
+        int high= n;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
 
             if (isBadVersion(mid)) {
 
-                right = mid;
+                high = mid;
             } else {
 
-                left = mid + 1;
+                low = mid + 1;
             }
         }
 
-        return left;
+        return low;
     }
 }
